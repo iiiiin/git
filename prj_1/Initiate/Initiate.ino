8184923eb9c698ffec3f333ad9aea731971a6b8b
iiiiin <keepa0041@naver.com>
2022-05-21 02:58:30 +0900

    220521

--- prj_1/Initiate/Initiate.ino
+++ prj_1/Initiate/Initiate.ino
@@ -1,10 +1,14 @@
 #include <SoftwareSerial.h>
-#include <Arduino.h>
+#include <Arduino.h>        
+
+// 헤더파일은 vscode 맞춤(무시)
 
 int blueTx = 6;
 int blueRx = 7;
 String i = "174";
 SoftwareSerial BTSerial(blueTx,blueRx);
+
+// 블루투스 모듈 핀 위치 선정, 변수 i 선언 (초기값 : 174)
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,27 +16,33 @@ void setup() {
   BTSerial.begin(9600);
 }
 
+// 시리얼, 블루투스 통신 보드레이트 맞춤
+
 void loop() {
   
-  String strCmd;
-  if(BTSerial.available())
+  String strCmd;            // strCmd : 블루투스로 입력받을 변수
+  if(BTSerial.available())  // 블루투스 페어링 상태일 경우
   {
-    strCmd = BTSerial.readString();
-    Serial.println(strCmd);
-    if(i == "600")
+    strCmd = BTSerial.readString(); // 블루투스로 입력받은 값 읽음
+    Serial.println(strCmd);         // 시리얼모니터에 strCmd 출력
+    if(i == "600")                  // i 값이 600 인 경우
     {
-      if(strCmd == "450")
+      if(strCmd == "450")           // 입력받은 strCmd 값이 450인 경우
       {
-        i = strCmd;
-        Serial.println(i);
-        Serial.println("d150");
-      }
+        i = strCmd;                 // strCmd 값을 i에 할당
+        Serial.println(i);          // 시리얼모니터에 할당된 i 값 출력
+        Serial.println("d150");     // d150 함수 실행 (으로 변경 예정)
+      }                             // d150 : 150mm 만큼 모터 역회전 함수
       else if(strCmd == "174")
       {
         i = strCmd;
         Serial.println(i);
         Serial.println("d426");
       }
+      else if(strCmd == "0")        // 입력받은 strCmd 값이 0인 경우
+      {
+        Serial.println("base");     // d426 함수 실행 (으로 변경 예정)
+      }                             // 각 조건에서 174mm로 가는 함수 실행      
     }
     else if(i == "450")
     {
@@ -47,6 +57,10 @@ void loop() {
         i = strCmd;
         Serial.println(i);
         Serial.println("d276");
+      }
+      else if(strCmd == "0")
+      {
+        Serial.println("base");
       }
     }
     else if(i == "174")
@@ -63,6 +77,90 @@ void loop() {
         Serial.println(i);
         Serial.println("u276");
       }
+      else if(strCmd == "0")
+      {
+        Serial.println("base");
+      }
     }
   }
 }
+
+//   위 언급한 함수(추가 예정)
+//   void up276() {
+//   myservo.write(80);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   analogWrite(PWM, 50);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   myservo.write(33.05);
+// }
+// void up426() {
+//   myservo.write(80);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   analogWrite(PWM, 50);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   myservo.write(44.8);
+// }
+// void up150() {
+//   myservo.write(80);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   analogWrite(PWM, 50);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, HIGH);
+//   delay(1000);
+//   myservo.write(44.8);
+// }
+// void dn276() {
+//   myservo.write(80);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   analogWrite(PWM, 50);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   myservo.write(8.68);
+// }
+// void dn426() {
+//   myservo.write(80);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   analogWrite(PWM, 50);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   myservo.write(8.68);
+// }
+// void dn150() {
+//   myservo.write(80);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   analogWrite(PWM, 50);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   analogWrite(PWM, 0);
+//   digitalWrite(DIR, LOW);
+//   delay(1000);
+//   myservo.write(33.05);
+// }
--- prj_1/bluetooth/bluetooth.ino
+++ prj_1/bluetooth/bluetooth.ino
@@ -1,125 +1,72 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
 #include <Servo.h>
-#include <EEPROM.h>
+
 
 //1번이 600, 2번이 450, 3번이 174.(각각44.8,33.05,8.68)
+
 int DIR = 4;
 int PWM = 5;
-int blueTx = 6;
+int blueTx = 6; 
 int blueRx = 7;
+int val_X;
+int val_Y;
+int val_Z;
+int buzzer = 12;
 String i = "174";
 SoftwareSerial BTSerial(blueTx,blueRx);
 Servo myservo;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
   BTSerial.begin(9600);
   pinMode(DIR, OUTPUT);
   pinMode(PWM, OUTPUT);
+  pinMode(buzzer,OUTPUT);
   myservo.attach(9);
   myservo.write(8.68);
-  BTSerial.println("Enter the Degrees :");
-}
-
-void up276() {
-  myservo.write(80);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  analogWrite(PWM, 50);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  myservo.write(33.05);
-}
-void up426() {
-  myservo.write(80);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  analogWrite(PWM, 50);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  myservo.write(44.8);
-}
-void up150() {
-  myservo.write(80);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  analogWrite(PWM, 50);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, HIGH);
-  delay(1000);
-  myservo.write(44.8);
-}
-void dn276() {
-  myservo.write(80);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  analogWrite(PWM, 50);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  myservo.write(8.68);
-}
-void dn426() {
-  myservo.write(80);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  analogWrite(PWM, 50);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  myservo.write(8.68);
-}
-void dn150() {
-  myservo.write(80);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  analogWrite(PWM, 50);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  analogWrite(PWM, 0);
-  digitalWrite(DIR, LOW);
-  delay(1000);
-  myservo.write(33.05);
-}
+  BTSerial.println("Enter the height you want :");
+  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(buzzer, HIGH);
+  val_X = analogRead(A0); 
+  val_Y = analogRead(A1);
+  val_Z = analogRead(A2);
+  Serial.print("X:");
+  Serial.print(val_X);
+  Serial.print("  Y:");
+  Serial.print(val_Y);
+  Serial.print("  Z:");
+  Serial.println(val_Z);
+  Serial.println();
   String strCmd;
+  // if(val_X > n || val_Y > n){
+  //   digitalWrite(buzzer,LOW);
+  //   delay(1000)
+  //   digitalWrite(buzzer,HIGH);
+  // }
   if(BTSerial.available())
   {
-    EEPROM.get(0,i);
     strCmd = BTSerial.readString();
     Serial.println(strCmd);
     if(i == "600")
     {
       if(strCmd == "450")
       {
-        dn150();
+        dist(-150);
         i = strCmd;
       }
       else if(strCmd == "174")
       {
-        dn426();
+        dist(-476);
+        i = strCmd;
+      }
+      else if(strCmd == "0")
+      {
+        dist(-476);
         i = strCmd;
       }
     }
@@ -127,12 +74,17 @@ void loop() {
     {
       if(strCmd == "600")
       {
-        up150();
+        dist(150);
         i = strCmd;
       }
       else if(strCmd == "174")
       {
-        dn276();
+        dist(-276);
+        i = strCmd;
+      }
+      else if(strCmd == "0")
+      {
+        dist(-276);
         i = strCmd;
       }
     }
@@ -140,15 +92,101 @@ void loop() {
     {
       if(strCmd == "600")
       {
-        up426();
+        dist(426);
         i = strCmd;
       }
       else if(strCmd == "450")
       {
-        up276();
+        dist(276);
         i = strCmd;
       }
     }
-    EEPROM.put(0,i);
+  }
+}
+
+void dist(int x) {
+  if(x == 426)
+  {
+    myservo.write(80);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    analogWrite(PWM, 255);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    myservo.write(44.8);
+  }
+  if(x == 276)
+  {
+    myservo.write(80);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    analogWrite(PWM, 255);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    myservo.write(33.05);
+  } 
+  if(x == 150)
+  {
+    myservo.write(80);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    analogWrite(PWM, 255);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, HIGH);
+    delay(500);
+    myservo.write(44.8);
+  }
+  if(x == -426)
+  {
+    myservo.write(80);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    analogWrite(PWM, 255);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    myservo.write(8.68);
+  }
+  if(x == -276)
+  {
+    myservo.write(80);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    analogWrite(PWM, 255);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    myservo.write(8.68);
+  }
+  if(x == -150)
+  {
+    myservo.write(80);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    analogWrite(PWM, 255);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    analogWrite(PWM, 0);
+    digitalWrite(DIR, LOW);
+    delay(500);
+    myservo.write(33.05);
   }
 }
